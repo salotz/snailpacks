fe5459641c6746ff4c7dd75e668b709f8c6427ff
salotz <salotz@salotz.info>
2022-03-08 01:04:08 -0500

    added chipmunk2d recipe

---
+++ packages/chipmunk2d/package.py
@@ -0,0 +1,27 @@
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Chipmunk2d(CMakePackage):
+    """A fast and lightweight 2D game physics library."""
+
+    homepage = "http://chipmunk-physics.net/"
+    url      = "https://github.com/slembcke/Chipmunk2D/archive/refs/tags/Chipmunk-7.0.3.tar.gz"
+
+    maintainers = ['salotz',]
+
+    version('7.0.3', sha256='1e6f093812d6130e45bdf4cb80280cb3c93d1e1833d8cf989d554d7963b7899a')
+
+    # FIXME: Add dependencies if required.
+    # depends_on('foo')
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
