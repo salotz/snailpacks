f2d9fa93f1516258b017866b2739a3a25f881399
salotz <salotz@salotz.info>
2022-04-01 22:30:39 -0400

    update scopes-chipmunk2d version
    0.1 was broken

--- packages/scopes-chipmunk2d/package.py
+++ packages/scopes-chipmunk2d/package.py
@@ -12,7 +12,9 @@ class ScopesChipmunk2d(Package):
 
     maintainers = ['salotz']
 
-    version('0.1', sha256='6e7a7d0288df8821c23672e703baa75dd1908461f3de97757e73d64e7fffc4b6')
+    version('0.2', sha256='c4a65d8a99e78be6b6ab66893eec98b95456666796b31cd4fce142c402768fc0')
+    # broken
+    # version('0.1', sha256='6e7a7d0288df8821c23672e703baa75dd1908461f3de97757e73d64e7fffc4b6')
 
     extends('scopes')
 
