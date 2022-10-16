51190d6bff3f9736a592c0f76fc55dabe8809dd4
salotz <salotz@salotz.info>
2022-10-16 17:36:56 -0400

    fixes chipmunk2d recipe
    - add options for shared vs static libraries
    - make shared libraries default to fix linking errors
    - variant for building demos to remove need for mesa dependency
    - add git URL for installing via has as version; which is needed for
      avoiding compilation errors on newish kernels

--- packages/chipmunk2d/package.py
+++ packages/chipmunk2d/package.py
@@ -1,4 +1,3 @@
-
 from spack import *
 
 
@@ -7,17 +6,25 @@ class Chipmunk2d(CMakePackage):
 
     homepage = "http://chipmunk-physics.net/"
     url      = "https://github.com/slembcke/Chipmunk2D/archive/refs/tags/Chipmunk-7.0.3.tar.gz"
+    git = "https://github.com/slembcke/Chipmunk2D.git"
 
     maintainers = ['salotz',]
 
     version('7.0.3', sha256='1e6f093812d6130e45bdf4cb80280cb3c93d1e1833d8cf989d554d7963b7899a')
 
-    # FIXME: Add dependencies if required.
-    # depends_on('foo')
+    variant("shared", default=True, description="Build shared libraries")
+    variant("static", default=False, description="Build as static libraries")
+
+    variant("demos", default=False, description="Whether to build and install the demos")
+
+    depends_on('mesa -llvm', when='+demos')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
-        args = []
+        args = [
+            self.define_from_variant('BUILD_DEMOS', 'demos'),
+            self.define_from_variant('INSTALL_DEMOS', 'demos'),
+            self.define_from_variant('BUILD_SHARED', 'shared'),
+            self.define_from_variant('BUILD_STATIC', 'static'),
+            self.define_from_variant('INSTALL_STATIC', 'static'),
+        ]
         return args
